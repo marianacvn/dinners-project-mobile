f43b0a940ead936242de6883d250211b9d000567
Mariana Carvalho <marianacvn1@gmail.com>
2022-08-04 23:26:41 -0300

    Criação da classe conexão de cliente com a Api

---
+++ lib/cliente.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Cliente {
+  String baseUrl = "http://127.0.0.1:8000/api/cliente/";
+
+  Future<List> listarCliente() async {
+    try {
+      var response = await http.get(Uri.parse(baseUrl));
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        return Future.error('Erro de servidor');
+      }
+    } catch (e) {
+      return Future.error(e);
+    }
+  }
+}
